9bcb71e17ec62daff53092bfb5aa7d2bc65491f5
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-09-19 16:50:57 -0500

    First commit, base project

---
+++ src/main/java/co/tripp/farrapp/api/data/Application.java
@@ -0,0 +1,18 @@
+package co.tripp.farrapp.api.data;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+
+}
---
+++ src/main/java/co/tripp/farrapp/api/data/model/User.java
@@ -0,0 +1,4 @@
+package co.tripp.farrapp.api.data.model;
+
+public class User {
+}
